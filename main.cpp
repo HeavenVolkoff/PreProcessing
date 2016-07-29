c78dee3c24a46cb6adf61a0aba32cad163d2e7d3
Vítor Augusto da Silva Vasconcellos <vasvas10@gmail.com>
2016-07-29 11:22:46 -0300

    Attempt at FFT % implemented fft but is probably bugged - removed unnecessary files

--- main.cpp
+++ main.cpp
@@ -8,14 +8,43 @@
 
 using namespace std;
 
+template <class T>
+void printVector (const vector<T> &v) {
+    cout << "[ ";
+    for (const auto &i : v) {
+        cout << i << ' ';
+    }
+    cout << ']' << endl;
+}
+
 inline
 double hann(double value) {
     double x = cos(value * M_PI / 2.0);
     return x*x;
 }
 
-vector<complex> fft (const vector<double> &data) {
-    // TODO: http://www.jimmo.org/example-of-one-dimensional-dft-of-real-data-with-fftw/
+vector<double> fft (vector<double> &audioData) {
+    // http://www.fftw.org/doc/Complex-numbers.html
+    // http://www.jimmo.org/example-of-one-dimensional-dft-of-real-data-with-fftw/
+    // https://stackoverflow.com/questions/15949833/usage-of-complex-numbers-in-c
+    int counter;
+    double real, imag;
+
+    vector<double> output(audioData.size());
+    fftw_complex* fftResult = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * audioData.size());
+    fftw_plan plan = fftw_plan_dft_r2c_1d((int) audioData.size(), audioData.data(), fftResult, FFTW_ESTIMATE);
+    fftw_execute(plan);
+
+    for (counter = 0; counter < audioData.size(); counter++) {
+        real = fftResult[counter][0];
+        imag = fftResult[counter][1];
+        output[counter] = sqrt((real * real) + (imag * imag));
+    }
+
+    fftw_destroy_plan(plan);
+    free(fftResult);
+
+    return output;
 }
 
 vector<double> getAudioDataFromWavFile (const std::string &filePath) {
@@ -53,10 +82,16 @@ int main(int argc, char **argv) {
 
     vector<double> audioData = getAudioDataFromWavFile(argv[1]);
 
+    printVector<double>(audioData);
+
     //Apply windowing function to vector
     transform(audioData.begin(), audioData.end(), audioData.begin(), &hann);
 
-    vector<complex> transformedData = fft(audioData);
+    printVector<double>(audioData);
+
+    vector<double> transformedData = fft(audioData);
+
+    printVector<double>(transformedData);
 
     return 0;
 }
