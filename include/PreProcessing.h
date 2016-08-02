28e1ce5a2b2cde911d663da40c2c20820966d893
Vítor Augusto da Silva Vasconcellos <vasvas10@gmail.com>
2016-08-02 00:15:44 -0300

    Added PreProcessing class Header

---
+++ include/PreProcessing.h
@@ -0,0 +1,15 @@
+#ifndef PREPROCESSING_PREPROCESSING_H
+#define PREPROCESSING_PREPROCESSING_H
+
+#include <iostream>
+
+using namespace std;
+
+template <size_t intervalLength, size_t strideLength>
+class PreProcessing {
+public:
+    PreProcessing(const string &);
+};
+
+
+#endif //PREPROCESSING_PREPROCESSING_H
