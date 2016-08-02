03172cafc220cc189f55ca23819f16dc8e9f77e3
Vítor Augusto da Silva Vasconcellos <vasvas10@gmail.com>
2016-08-02 01:52:39 -0300

    Some TODO's and reference

--- include/PreProcessing.h
+++ include/PreProcessing.h
@@ -5,10 +5,31 @@
 
 using namespace std;
 
-template <size_t intervalLength, size_t strideLength>
+
+/**
+ * @link: http://support.ircam.fr/docs/AudioSculpt/3.0/co/FFT%20Size.html
+ * @link: http://www.fftw.org/doc/Real_002dto_002dReal-Transforms.html#Real_002dto_002dReal-Transforms
+ *
+ * Propriedades:
+ * frameLength
+ * frameStep
+ * OversamplingFactor
+ * fftSize = primeira potencia de (2 * OversamplingFactor) igual ou maior ao frameLength
+ * binSize = fftSize/2
+ */
+
+template <size_t frameLength, size_t frameStep>
 class PreProcessing {
 public:
-    PreProcessing(const string &);
+    PreProcessing();
+    ~PreProcessing();
+
+    /**
+     * TODO # This is the PreProcessing class, it should be created only once.
+     * TODO # It should have methods for loading audio both from WAV files (libsnd) and input devices (portaudio).
+     * TODO # It will apply to the audio data framming¹ (cut and stride) as well as windowing (border easing).
+     * TODO # It will apply FFT (fftw) and MFCC to each audio data fragment
+     */
 };
 
 
